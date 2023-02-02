17e92fbeca5bf5e7c78910f51b0d98d77d6a4df1
Altaf Ahmed <90945964+AltafAhmedGeek@users.noreply.github.com>
2023-02-03 00:38:21 +0530

    Create EvenOddSum.java

---
+++ Lab/02-02-2023_Lab1/EvenOddSum.java
@@ -0,0 +1,58 @@
+
+public class EvenOddSum {
+	public void evenNums()
+	{
+		for(int i=1;i<=200;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+			
+			
+		}
+		System.out.println("");
+	}
+	public void oddNums()
+	{
+		for(int i=1;i<=200;i++)
+		{
+			if(i%2!=0)
+			{
+				System.out.print(i+" ");
+			}
+			
+			
+		}
+		System.out.println("");
+	}
+	public void sumNums()
+	{
+		int sum=0;
+		for(int i=1;i<=200;i++)
+		{
+			
+				sum=sum+i;
+			
+			
+			
+		}
+		System.out.print(sum);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		
+		EvenOddSum obj= new EvenOddSum();
+		System.out.println("Even Numbers from 1 to 200 :");
+		obj.evenNums();
+		System.out.println("Odd Numbers from 1 to 200 :");
+		obj.oddNums();
+		System.out.println("Sum of Numbers from 1 to 200 :");
+		obj.sumNums();
+
+
+	}
+
+}
