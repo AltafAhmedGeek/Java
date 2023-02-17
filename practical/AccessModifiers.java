405dca5a646909836250b05822112d5299b55784
Altaf Ahmed <90945964+AltafAhmedGeek@users.noreply.github.com>
2023-02-17 07:05:06 +0530

    example of private and protected access modifier

---
+++ practical/AccessModifiers.java
@@ -0,0 +1,15 @@
+
+public class AccessModifiers extends Person {
+	private String num1 = "Sam";
+
+	public static void main(String[] args) {
+		AccessModifiers am = new AccessModifiers();
+		System.out.println("Name: " + am.name);
+		System.out.println("Num: " + am.num);
+		System.out.println("Friend Name: " + am.num1);
+	}
+}
+class Person {
+	protected String name = "Ram";
+	protected long num = 8888555;
+}
