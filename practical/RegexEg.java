d2e8d11f5327b9e236cc5d0144e59cd03cd8c81a
Altaf Ahmed <90945964+AltafAhmedGeek@users.noreply.github.com>
2023-03-10 17:31:15 +0530

    Add files via upload

---
+++ practical/RegexEg.java
@@ -0,0 +1,12 @@
+import java.util.regex.Pattern;
+
+public class RegexEg {
+
+	public static void main(String[] args) {
+		boolean b=Pattern.matches("[a-z1-9]{5,}", "hello2");
+		System.out.println(b);
+		b=Pattern.matches("[a-z1-9]{5,}", "Hel");
+		System.out.println(b);
+	}
+
+}
